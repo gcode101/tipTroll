0c45380f67ad1b8e32602532b03ad9d6cd5eaa06
Guelmis Cortina <guelmiscortina@Guelmis-Cortinas-MacBook-Pro.local>
2015-11-10 18:07:07 -0500

    plugged several markers on the map with their info window

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -2,71 +2,65 @@
 var ready = function(){
   //Start geolocation
 
-    var proId = []; //returned from the API
-    var allLatlng = []; //returned from the API
-    var allMarkers = []; //returned from the API
-    var proName = []; //returned from the API
-    var infowindow = null;
-    var pos;
-    var userCords;
-    var tempMarkerHolder = [];
+  var proId = []; //returned from the API
+  var allLatlng = []; //returned from the API
+  var allMarkers = []; //returned from the API
+  var proName = []; //returned from the API
+  var infowindow = null;
+  var pos;
+  var userCords;
+  var tempMarkerHolder = [];
 
-    if (navigator.geolocation) {
+  if (!document.getElementById('map-canvas')) {
+    return false;
+  }
 
-      function error(err) {
-        console.warn('ERROR(' + err.code + '): ' + err.message);
-      }
+  //Fire up Google maps and place inside the map-canvas div
+  var mapOptions = {
+    zoom: 5,
+    center: new google.maps.LatLng(37.09024, -100.712891),
+    panControl: false,
+    panControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_LEFT
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.LARGE,
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    },
+    scaleControl: false
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-      function success(pos){
-        userCords = pos.coords;
-        //return userCords;
-      }
 
-      // Get the user's current position
-      navigator.geolocation.getCurrentPosition(success, error);
-      // console.log(pos.latitude + " " + pos.longitude);
-      } else {
-        alert('Geolocation is not supported in your browser');
-      }
+  if (navigator.geolocation) {
+    function error(err) {
+      console.warn('ERROR(' + err.code + '): ' + err.message);
+    }
 
-    //End Geo location
+    function success(pos){
+      userCords = pos.coords;
+      console.log(userCords);
+      //return userCords;
+    }
 
-    //map options
-    var mapOptions = {
-      zoom: 5,
-      center: new google.maps.LatLng(37.09024, -100.712891),
-      panControl: false,
-      panControlOptions: {
-        position: google.maps.ControlPosition.BOTTOM_LEFT
-      },
-      zoomControl: true,
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.LARGE,
-        position: google.maps.ControlPosition.RIGHT_CENTER
-      },
-      scaleControl: false
-
-    };
+    // Get the user's current position
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert('Geolocation is not supported in your browser');
+  }
+  //End Geo location
 
   //Adding infowindow option
   infowindow = new google.maps.InfoWindow({
     content: "holding..."
   });
 
-  if (!document.getElementById('map-canvas')) {
-    return false;
-  }
-
-
-  //Fire up Google maps and place inside the map-canvas div
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    //grab form data
-    $('#chooseZip').submit(function() { // bind function to submit event of form
+  //grab form data
+  $('#chooseZip').submit(function() { // bind function to submit event of form
 
     //define and set variables
     var userZip = $("#textZip").val();
-    //console.log("This-> " + userCords.latitude);
 
     var accessURL;
 
@@ -76,67 +70,48 @@ var ready = function(){
       accessURL = "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/locSearch?lat=" + userCords.latitude + "&lng=" + userCords.longitude;
     }
 
+    return false; // important: prevent the form from submitting
+  }); // end submit
 
-      //Use the zip code and return all market ids in area.
-      $.ajax({
-        type: "GET",
-        contentType: "application/json; charset=utf-8",
-        url: accessURL,
-        data: {
-          zipCode: userZip,
-        } ,
-        success: function (data) {
-          console.log(data);
-          var counter = 0;
-          for (var person in data) {
-
-                //set the markers.
-                geocoder = new google.maps.Geocoder();
+  // $('.location').each(function(idx, item) {
+  //   add_marker(map, $(item).text());
+  // });
 
 
-                  var address = '3468 Foxcroft Rd Miramar FL 33025';
+  function add_marker(map, address) {
+    new google.maps.Geocoder().geocode( { 'address': address }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var latitude = results[0].geometry.location.lat();
+        var longitude = results[0].geometry.location.lng();
+        map.setCenter(results[0].geometry.location);
+        map.setZoom(13);
 
-                  geocoder.geocode( { 'address': address}, function(results, status) {
-                   if (status == google.maps.GeocoderStatus.OK) {
-                      var latitude = results[0].geometry.location.lat();
-                      var longitude = results[0].geometry.location.lng();
-                      map.setCenter(results[0].geometry.location);
-                      map.setZoom(10);
+        var myLatlng = new google.maps.LatLng(latitude,longitude);
 
-                      myLatlng = new google.maps.LatLng(latitude,longitude);
+        //set the markers.
+        var allMarkers = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: "Barbers",
+          html:
+          '<div class="markerPop">' +
+          '<h1>' + "Barbers" + '</h1>' +
+          '<h3>' + results['Address'] + '</h3>' +
+          '<p>' + results['Products'] + '</p>' +
+          '<p>' + results['Schedule'] + '</p>' +
+          '</div>'
+        });
 
-                      allMarkers = new google.maps.Marker({
-                        position: myLatlng,
-                        map: map,
-                        title: "Barbers",
-                        html:
-                            '<div class="markerPop">' +
-                            '<h1>' + "Barbers" + '</h1>' +
-                           '<h3>' + results['Address'] + '</h3>' +
-                            '<p>' + results['Products'] + '</p>' +
-                            '<p>' + results['Schedule'] + '</p>' +
-                            '</div>'
-                      });
+          google.maps.event.addListener(allMarkers, 'click', function () {
+            infowindow.setContent(this.html);
+            infowindow.open(map, this);
+          });
 
-                      counter++;
-                      console.log(counter);
-                    } else {
-                      alert("Geocode was not successful for the following reason: " + status);
-                    }
-                  });
-              };
-
-                google.maps.event.addListener(allMarkers, 'click', function () {
-                  infowindow.setContent(this.html);
-                  infowindow.open(map, this);
-                });
-
-              }
-            });
-
-            return false; // important: prevent the form from submitting
-          }); //end .each
-
+        } else {
+          alert("Geocode was not successful for the following reason: " + status);
+        }
+      }); // end geocoder
+    }
 }
 
 $(document).ready(ready);
