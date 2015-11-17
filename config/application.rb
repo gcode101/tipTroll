8a695f8d26cff3094313f7ecb572de0dda4795d6
Guelmis Cortina <guelmiscortina@Guelmis-Cortinas-MacBook-Pro.local>
2015-11-17 18:34:15 -0500

    fixing heroku issues with the images

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module TipTroll
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.enabled = true
+    # config.assets.enabled = true
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    # config.serve_static_assets = true
   end
 end
